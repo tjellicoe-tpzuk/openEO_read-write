8ffc5a6bdfc79acee289f3a7a3677e2700fa64c8
tjellicoe-tpzuk <149666043+tjellicoe-tpzuk@users.noreply.github.com>
2023-12-01 17:05:58 +0000

    Add files via upload

--- Download from Git/import_netcdf.py
+++ Download from Git/import_netcdf.py
@@ -1,12 +1,19 @@
 import xarray as xr
 import requests
 import json
+from openeo.udf import XarrayDataCube
 
 def read_netcdf_from_git(url: str):
     loaded_file_raw = requests.get(url)
     loaded_da = xr.load_dataarray(loaded_file_raw.content)
     print(loaded_da)
+    return loaded_da
 
 url = "https://raw.githubusercontent.com/tjellicoe-tpzuk/openEO_read-write/main/data/outfile.nc"
 #url = "https://github.com/tjellicoe-tpzuk/openEO_read-write/blob/main/data/outfile.nc"
 read_netcdf_from_git(url)
+
+
+def apply_datacube(cube: XarrayDataCube, context: dict):
+    url = context['url']
+    return read_netcdf_from_git(url)
--- Upload to Git/upload_STAC.py
+++ Upload to Git/upload_STAC.py
@@ -2,25 +2,34 @@ from openeo.udf import XarrayDataCube
 import xarray as xr
 from github import Github
 import rioxarray
+from git_access import git_access_token
+from datetime import datetime as dt
+import time
 
 def apply_datacube(cube: XarrayDataCube, context: dict):
     xarrayTest = cube.to_array()
-    create_STAC(xarrayTest)
+    access_token = "please_insert_token"
+    url = create_STAC(xarrayTest, access_token)
+    return url
 
 
-
-def create_STAC(cube: xr.DataArray):
-    g = Github('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')
-    
+def create_STAC(cube: xr.DataArray, access_token: str):
+    g = Github(access_token)
     repo = g.get_repo('tjellicoe-tpzuk/openEO_read-write')
     outfile = cube.to_netcdf()
-    repo.create_file('data/outfile.nc', 'upload tif', outfile, branch='main')
+    todayDate = dt.now().strftime("%y-%m-%d")
+    timeNow = time()[-6:]
+    fileName = f"openEO_output_{timeNow}"
+    repo.create_file(f'data/{fileName}.nc', 'upload tif', outfile, branch='main')
+    #print(repo)
+    url = "https://raw.githubusercontent.com/tjellicoe-tpzuk/openEO_read-write/main/data/" + fileName + ".nc"
+    
+    return url
 
 
-# if __name__ == "__main__":
+if __name__ == "__main__":
+    geotiff_path_red = "/home/tjellicoe/Documents/EOEPCA-and-OPENEO/useful scripts/B01.tif"
 
-#     geotiff_path_red = "/home/tjellicoe/Documents/EOEPCA-and-OPENEO/useful scripts/B01.tif"
-
-#     xarrayTest = rioxarray.open_rasterio(geotiff_path_red).to_dataset('band').to_dataarray()
-
-#     create_STAC(xarrayTest)
+    xarrayTest = rioxarray.open_rasterio(geotiff_path_red).to_dataset('band').to_dataarray()
+    print(dt.now().strftime("%y-%m-%d"))
+    create_STAC(xarrayTest, git_access_token)
